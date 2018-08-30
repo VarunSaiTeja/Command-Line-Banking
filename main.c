2e33b1613c8a367cd73ccbe79ba781bfe8ac4a1d
VarunSaiTeja <35555010+VarunSaiTeja@users.noreply.github.com>
2018-08-30 14:55:26 +0530

    Delete Database
    delete_db argument will now remove database from application startup (running) path

--- main.c
+++ main.c
@@ -4,12 +4,18 @@
 #include <time.h>
 
 void create_db();
+void delete_db();
 
 int main(int arg_count,char* arguments[])
 {
     if(strcmp(arguments[1],"create_db")==0)
     {
         create_db();
+    }
+
+    if(strcmp(arguments[1],"delete_db")==0)
+    {
+        delete_db();
     }
 
     return 0;
@@ -23,4 +29,11 @@ void create_db()
     fclose(new_file);
 
     printf("database created");
+}
+
+void delete_db()
+{
+    remove("accounts.txt");
+
+    printf("database deleted");
 }
