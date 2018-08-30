82df0b4ff6f81dfa2d0dab073768881c942579a2
VarunSaiTeja <35555010+VarunSaiTeja@users.noreply.github.com>
2018-08-30 15:21:59 +0530

    Create Account
    create_account follows many arguments
    so please view README.md

--- main.c
+++ main.c
@@ -5,6 +5,7 @@
 
 void create_db();
 void delete_db();
+void create_account(char first_name[],char last_name[],char account_no[],char mail[],int balance,int pin,char city[]);
 
 int main(int arg_count,char* arguments[])
 {
@@ -16,6 +17,20 @@ int main(int arg_count,char* arguments[])
     if(strcmp(arguments[1],"delete_db")==0)
     {
         delete_db();
+    }
+
+    if(strcmp(arguments[1],"create_account")==0)
+    {
+        //arguments=program_name,create_account,first_name,last_name,mobile,email,balance,password,city
+        //total arguments=9
+        if(arg_count==9)
+        {
+            create_account(arguments[2],arguments[3],arguments[4],arguments[5],atoi(arguments[6]),atoi(arguments[7]),arguments[8]);
+        }
+        else
+        {
+            printf("invalid arguments");
+        }
     }
 
     return 0;
@@ -36,4 +51,17 @@ void delete_db()
     remove("accounts.txt");
 
     printf("database deleted");
+}
+
+void create_account(char first_name[],char last_name[],char account_no[],char mail[],int balance,int pin,char city[])
+{
+    SYSTEMTIME time;
+    GetLocalTime(&time);
+
+    FILE *accounts;
+    accounts=fopen("accounts.txt","a");
+    fprintf(accounts,"%s %s %s %s %d %d %s\n",first_name,last_name,account_no,mail,balance,pin,city);
+    fclose(accounts);
+
+    printf("account created");
 }
