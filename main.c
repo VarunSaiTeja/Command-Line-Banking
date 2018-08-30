f9df80c9ff9b819de1b0bd660787a9c91db45b77
VarunSaiTeja <35555010+VarunSaiTeja@users.noreply.github.com>
2018-08-30 14:49:24 +0530

    Create Database
    create_db argument will now create database in application startup (running) path

--- main.c
+++ main.c
@@ -3,7 +3,24 @@
 #include <windows.h>
 #include <time.h>
 
+void create_db();
+
 int main(int arg_count,char* arguments[])
 {
+    if(strcmp(arguments[1],"create_db")==0)
+    {
+        create_db();
+    }
+
     return 0;
+}
+
+void create_db()
+{
+    FILE *new_file;
+
+    new_file=fopen("accounts.txt","w");
+    fclose(new_file);
+
+    printf("database created");
 }
