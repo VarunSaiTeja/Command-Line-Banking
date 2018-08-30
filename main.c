73d0df165565599f41bd1ca4db00fd823f97c4a4
VarunSaiTeja <35555010+VarunSaiTeja@users.noreply.github.com>
2018-08-30 19:32:44 +0530

    Deposit Amount
    deposit _amount followed by arguments account_no and amount will deposit amount into account and saves database
    
    View README.md for mor details

--- main.c
+++ main.c
@@ -7,6 +7,7 @@ void create_db();
 void delete_db();
 void create_account(char first_name[],char last_name[],char account_no[],char mail[],int balance,int pin,char city[]);
 void close_account(char account_no[]);
+void deposit(char account_no[],int amount);
 
 struct account
 {
@@ -59,6 +60,17 @@ int main(int arg_count,char* arguments[])
         }
     }
 
+    if(strcmp(arguments[1],"deposit")==0)
+    {
+        if(arg_count==4)
+        {
+            deposit(arguments[2],atoi(arguments[3]));
+        }
+        else
+        {
+            printf("invalid arguments");
+        }
+    }
     return 0;
 }
 
@@ -144,6 +156,52 @@ void close_account(char account_no[])
         remove("accounts.txt");
         rename("temp.txt","accounts.txt");
         printf("account closed");
+    }
+    else
+    {
+        printf("account not found");
+    }
+}
+
+void deposit(char account_no[],int amount)
+{
+    int account_found=FALSE;
+    FILE *accounts,*temp;
+
+    accounts=fopen("accounts.txt","r");
+    while(!feof(accounts))
+    {
+        fscanf(accounts,"%s %s %s %s %d %d %s\n",user.first_name,user.last_name,user.account_no,user.mail,&user.balance,&user.pin,user.city);
+        if(strcmp(user.account_no,account_no)==0)
+        {
+            account_found=TRUE;
+        }
+    }
+    fclose(accounts);
+
+    if(account_found==TRUE)
+    {
+        accounts=fopen("accounts.txt","r");
+        temp=fopen("temp.txt","w");
+
+        while(!feof(accounts))
+        {
+            fscanf(accounts,"%s %s %s %s %d %d %s\n",user.first_name,user.last_name,user.account_no,user.mail,&user.balance,&user.pin,user.city);
+            if(strcmp(user.account_no,account_no)!=0)
+            {
+                fprintf(temp,"%s %s %s %s %d %d %s\n",user.first_name,user.last_name,user.account_no,user.mail,user.balance,user.pin,user.city);
+            }
+            else
+            {
+                fprintf(temp,"%s %s %s %s %d %d %s\n",user.first_name,user.last_name,user.account_no,user.mail,(user.balance+amount),user.pin,user.city);
+            }
+        }
+
+        fclose(accounts);
+        fclose(temp);
+        remove("accounts.txt");
+        rename("temp.txt","accounts.txt");
+        printf("amount deposited");
     }
     else
     {
